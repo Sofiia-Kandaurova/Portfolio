c7a7f8f8a3b87b95b19183b3d31e33f64483275b
Anton-Kandaurov <115020468+Anton-Kandaurov@users.noreply.github.com>
2026-02-03 18:45:07 +0900

    add japanese version

--- js/lang-modal.js
+++ js/lang-modal.js
@@ -2,23 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("welcomeModal");
   const closeBtn = document.getElementById("closeModal");
 
-  // Если модалки нет на странице — ничего не делаем
   if (!modal || !closeBtn) return;
 
-  const KEY = "GLOBAL_WELCOME_MODAL";
+  const KEY = "WELCOME_MODAL_SESSION";
 
-  // Если уже показывали — не открываем
-  if (localStorage.getItem(KEY) === "true") {
+  if (sessionStorage.getItem(KEY) === "true") {
     modal.style.display = "none";
     return;
   }
 
-  // Первый и единственный показ
   modal.style.display = "flex";
 
-  // КНОПКА ЗАКРЫТИЯ
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
-    localStorage.setItem(KEY, "true");
+    sessionStorage.setItem(KEY, "true");
   });
 });
